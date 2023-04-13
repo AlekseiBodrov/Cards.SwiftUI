e9fd3b5465498c85ceddd0bdae42ffca8c7aca49
Aleksei Bodrov <culls.03valleys@icloud.com>
2023-04-13 12:32:26 +0300

    Initial commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Алексей Бодров on 13.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
